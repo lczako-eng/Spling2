9cef329bdee7f3c97aee07e977f70710fe30c561
Laszlo Czako <l.czako@icloud.com>
2024-10-05 11:48:08 -0400

    Initial Commit

---
+++ Spling1/Spling1App.swift
@@ -0,0 +1,17 @@
+//
+//  Spling1App.swift
+//  Spling1
+//
+//  Created by Laszlo Czako on 2024-10-05.
+//
+
+import SwiftUI
+
+@main
+struct Spling1App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Spling1Tests/Spling1Tests.swift
@@ -0,0 +1,17 @@
+//
+//  Spling1Tests.swift
+//  Spling1Tests
+//
+//  Created by Laszlo Czako on 2024-10-05.
+//
+
+import Testing
+@testable import Spling1
+
+struct Spling1Tests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
